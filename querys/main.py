508beff40cc08cfb9787852c1e04f7ced58dc924
patxijuaristi <patxi585@gmail.com>
2023-04-20 17:31:09 +0200

    Dotenv included

--- querys/main.py
+++ querys/main.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
 from querys import get_avg_temperature
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Read the Mongo connection string from the environment variables and create the client
 client = MongoClient(os.environ['MONGO_CLIENT'])
